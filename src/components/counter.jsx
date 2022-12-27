98c7b0a3ac1ef3c8870521c98d77ff3d8ef55fc3
Igor <nhdigor@gmail.com>
2022-12-27 18:59:16 +0300

    the end of the third lesson

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Counter = () => {
-  return <h1>Counter!</h1>
+  return (
+    <>
+      <h1>Counter!</h1>
+      <button>+</button>
+    </>
+  )
 }
 
 export default Counter
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react' //импортируем React
-import reactDom from 'react-dom' //импортируем ReactDom
-import 'bootstrap/dist/css/bootstrap.css' //импортируем стили CSS
+import React from 'react'
+import reactDom from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
 
 import Counter from './components/counter'
 
