0b7ccb1fd80fddfaab060c0d592f1448ec2a7ac8
Igor <nhdigor@gmail.com>
2022-12-29 22:29:04 +0300

    the end of the fourth lesson

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Counter = () => {
+  const count = 2 - 2
+  const formatCount = () => {
+    return count === 0 ? <h1>empty</h1> : count
+  }
   return (
     <>
-      <h1>Counter!</h1>
+      <span>{formatCount()}</span>
       <button>+</button>
     </>
   )
